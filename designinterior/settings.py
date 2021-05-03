b549df53b0a9efe53a9eebae6144d223993ec09d
StefanoS <iiostefanos@gmail.com>
2021-05-03 14:41:05 +0000

    Removed secret key

--- designinterior/settings.py
+++ designinterior/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8w1cdn*8e^)f(24q2kfta*na-1op0&2^-e+yj$m-9c_^5!zgs'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
